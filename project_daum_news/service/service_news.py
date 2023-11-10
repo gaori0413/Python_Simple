172724ae9b569b09345f54cb1210a8e834e3b910
gaori0413 <134304862+gaori0413@users.noreply.github.com>
2023-11-10 14:32:22 +0900

    Add files via upload

--- project_daum_news/service/service_news.py
+++ project_daum_news/service/service_news.py
@@ -1,6 +1,7 @@
 # 해당 URL 접속해서 기사 [제목, 본문, 날짜] 수집
 import requests
 from bs4 import BeautifulSoup
+from db.news_dao import add_news
 
 def get_news(url: str):
     result = requests.get(url)
@@ -23,3 +24,11 @@ def get_news(url: str):
 
     print(f"본문: {content}")
 
+# 수집한 데이터 DB에 저장
+# MongoDB → JSON = Dict Type
+    data = {
+        "title": title,
+        "content": content,
+        "date": reg_date,
+    }
+    add_news(data)
